f1b4d11dc4b5e3bcfb4f70d94d61d271f143d2a0
wasawatkp <wasawat.upper@gmail.com>
2025-01-28 01:52:32 +0700

    lab14_1 clearrrr

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -4,6 +4,34 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 
+	 for(int i=1;i<N;i++){
+	 	T x = d[i];
+		int j ;
+
+		/*cout << "i = " << i << ": ";
+		cout << "\n";*/
+
+		for (j = i - 1; j >= 0; j--) {
+			//cout << "j = " << j << " ";
+    		if (d[j] < x) { 
+        		d[j+1] = d[j]; 
+				
+    			} 
+			else  break;
+        		}
+		d[j+1] = x;
+		//cout << "\n";
+
+		cout << "Pass " << i << ":" ;
+
+		for(int k=0;k<N;k++){
+			cout << d[k] << " ";
+		}
+		cout << "\n";
+	 }
+
+		
+
 }
 
 int main(){
